95b583758d865bc4d9abbf1337326a2347d36153
Sma1lo <heee58411@gmail.com>
2025-12-28 18:34:23 +0300

    create project

---
+++ src/main/java/com/cofy/CofyApplication.java
@@ -0,0 +1,13 @@
+package com.cofy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CofyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CofyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cofy/CofyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cofy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CofyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
